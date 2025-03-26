1e76eb5b357c9eac8613155852f8edaf09c7827c
AyushJoshi1403 <ayush.aj1403@gmail.com>
2025-03-26 13:50:07 +0530

    navbar update

--- frontend/src/app/(main)/Navbar.jsx
+++ frontend/src/app/(main)/Navbar.jsx
@@ -8,13 +8,13 @@ const Navbar = () => {
         <header className="flex flex-wrap  md:justify-start md:flex-nowrap z-50 w-full bg-white border-b border-gray-200 dark:bg-neutral-800 dark:border-neutral-700">
           <nav className="relative max-w-[85rem] w-full mx-auto md:flex md:items-center md:justify-between md:gap-3 py-2 px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between items-center gap-x-1">
-              <a
+              <Link
                 className="flex-none font-semibold text-xl text-black focus:outline-hidden focus:opacity-80 dark:text-white"
-                href="#"
+                href="./page.jsx"
                 aria-label="Brand"
               >
                 Electoral Voting System
-              </a>
+              </Link>
               {/* Collapse Button */}
               <button
                 type="button"
