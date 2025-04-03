dad675ea02fe1b05c6df0fe40856472d323ea0da
AyushJoshi1403 <ayush.aj1403@gmail.com>
2025-04-04 05:08:34 +0530

    project

--- frontend/src/app/(main)/Navbar.jsx
+++ frontend/src/app/(main)/Navbar.jsx
@@ -190,12 +190,12 @@ const Navbar = () => {
                                   >
                                     Downloads
                                   </a>
-                                  <a
+                                  <Link
                                     className="p-2 md:px-3 flex items-center text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
                                     href="#"
                                   >
                                     Team Account
-                                  </a>
+                                  </Link>
                                 </div>
                               </div>
                             </div>
