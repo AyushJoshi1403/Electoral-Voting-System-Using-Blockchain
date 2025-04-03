5ce9b8fb40ce7dade9bb5c67c9988028633a7fe0
AyushJoshi1403 <ayush.aj1403@gmail.com>
2025-04-03 23:30:27 +0530

    update

--- frontend/src/app/(main)/Navbar.jsx
+++ frontend/src/app/(main)/Navbar.jsx
@@ -178,12 +178,12 @@ const Navbar = () => {
                                 aria-labelledby="hs-header-base-dropdown-sub"
                               >
                                 <div className="p-1 space-y-1">
-                                  <a
+                                  <Link
                                     className="p-2 md:px-3 flex items-center text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
                                     href="#"
                                   >
                                     About
-                                  </a>
+                                  </Link>
                                   <a
                                     className="p-2 md:px-3 flex items-center text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
                                     href="#"
