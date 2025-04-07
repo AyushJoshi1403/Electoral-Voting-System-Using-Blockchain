c8bc826a30e2a6348301d9cbcaa33989950ec088
AyushJoshi1403 <ayush.aj1403@gmail.com>
2025-04-08 00:01:55 +0530

    update

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -60,6 +60,8 @@ function Home() {
 
     try {
       if (window.ethereum) {
+        // console.log(window.ethereum);
+        
         await window.ethereum.request({ method: 'eth_requestAccounts' });
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
